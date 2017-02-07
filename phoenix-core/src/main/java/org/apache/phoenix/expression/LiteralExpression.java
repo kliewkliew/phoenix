5702c1b90119b41d56014c3621e95c0cfe869a31
kliewkliew <kliew@apache.org>
2017-02-06 19:53:58 -0800

    PHOENIX-3413 Ineffective null check in LiteralExpression#newConstant()

--- phoenix-core/src/main/java/org/apache/phoenix/expression/LiteralExpression.java
+++ phoenix-core/src/main/java/org/apache/phoenix/expression/LiteralExpression.java
@@ -183,6 +183,7 @@ public class LiteralExpression extends BaseTerminalExpression {
             return getBooleanLiteralExpression((Boolean)value, determinism);
         }
         PDataType actualType = PDataType.fromLiteral(value);
+        type = type == null ? actualType : type;
         try {
             value = type.toObject(value, actualType);
         } catch (IllegalDataException e) {
@@ -207,7 +208,7 @@ public class LiteralExpression extends BaseTerminalExpression {
             return getTypedNullLiteralExpression(type, determinism);
         }
         if (maxLength == null) {
-            maxLength = type == null || !type.isFixedWidth() ? null : type.getMaxLength(value);
+            maxLength = type.isFixedWidth() ? type.getMaxLength(value) : null;
         }
         return new LiteralExpression(value, type, b, maxLength, scale, sortOrder, determinism);
     }
